860b0b435bebd1393b04a0cafb86b6ef963e1336
Jan Decavele <jandecavele@gmail.com>
2017-11-09 09:52:37 +0100

    Removed depricated pattern import

--- DjangoRocket/urls.py
+++ DjangoRocket/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^blog/', include('blog.urls')),
-
      url(r'^fire', 'DjangoRocket.views.fire', name='fire'),
      url(r'^move', 'DjangoRocket.views.move', name='move'),
      url(r'^abort', 'DjangoRocket.views.abort', name='abort'),
      url(r'^load', 'DjangoRocket.views.loadUp', name='load'),
      url(r'^files', 'DjangoRocket.views.files', name='files'),
      url(r'^$', 'DjangoRocket.views.home', name='home'),
-)
+]
